0da4882cf2e3971a951f003a22bd5ca0a7acfc70
Michael Han <michael_hll@hotmail.com>
2021-05-11 15:19:25 +0800

    Commit ImageCropper solution
    commit image cropper solution files
    before it's been added into the ignore list

---
+++ ImageCropper.Forms-master/Test/Test/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Stormlion.ImageCropper;
+using System;
+using Xamarin.Forms;
+
+namespace Test
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+        }
+
+        protected async void OnClickedRectangle(object sender, EventArgs e)
+        {
+            imageView.Source = null;
+            new ImageCropper()
+            {
+//                PageTitle = "Test Title",
+//                AspectRatioX = 1,
+//                AspectRatioY = 1,
+                Success = (imageFile) =>
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        imageView.Source = ImageSource.FromFile(imageFile);
+                    });
+                },
+                Faiure = () => {
+                    Console.WriteLine("Error capturando la imagen o haciendo crop.");
+                }
+            }.Show(this);
+        }
+
+        private void OnClickedCircle(object sender, EventArgs e)
+        {
+            imageView.Source = null;
+            new ImageCropper()
+            {
+                CropShape = ImageCropper.CropShapeType.Oval,
+                Success = (imageFile) =>
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        imageView.Source = ImageSource.FromFile(imageFile);
+                    });
+                },
+                Faiure = () => {
+                    Console.WriteLine("Error capturando la imagen o haciendo crop.");
+                }
+            }.Show(this);
+        }
+    }
+}
